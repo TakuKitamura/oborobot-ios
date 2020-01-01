1d1764925017b735c2d5b74bfb918fd29d3e867e
TakuKitamura <takukitamura.io@gmail.com>
2020-01-01 15:17:42 +0900

    Initial Commit

---
+++ oborobot-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  oborobot-ios
+//
+//  Created by 喜多村卓 on 2020/01/01.
+//  Copyright © 2020 喜多村卓. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
